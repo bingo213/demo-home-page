5d0933d8b712d09c37b5cfb09d4ba3bb3f3cf435
Trần Thị Hoa Hiên <cassiopeiahien@gmail.com>
2020-10-11 11:37:49 +0700

    push

---
+++ home.js
@@ -0,0 +1,18 @@
+// function myFunction(x) {
+//   if (x.matches) {
+//     // If media query matches
+//     document.getElementById('head').classList.remove('large-header');
+//     document.getElementById('head').classList.add('small-header');
+//     document.getElementById('nav').classList.add('nav-container');
+//     document.getElementById('nav').classList.remove('large-container');
+//   } else {
+//     document.getElementById('head').classList.add('large-header');
+//     document.getElementById('head').classList.remove('small-header');
+//     document.getElementById('nav').classList.remove('nav-container');
+//     document.getElementById('nav').classList.add('large-container');
+//   }
+// }
+
+// var x = window.matchMedia('(max-width: 800px)');
+// myFunction(x); // Call listener function at run time
+// x.addListener(myFunction); // Attach listener function on state changes
