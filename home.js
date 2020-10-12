099fdf59236c5c05538f9f54d925116ffd3598d5
Trần Thị Hoa Hiên <cassiopeiahien@gmail.com>
2020-10-12 12:10:42 +0700

    gi cha dc

--- home.js
+++ home.js
@@ -16,3 +16,21 @@
 // var x = window.matchMedia('(max-width: 800px)');
 // myFunction(x); // Call listener function at run time
 // x.addListener(myFunction); // Attach listener function on state changes
+
+// var arr = [
+//   './images/slide image 1.jpg',
+//   './images/slide image 2.jpg',
+//   './images/slide image 3.jpg',
+//   './images/slide image 4.jpg',
+//   './images/slide image 5.jpg'
+// ];
+
+// index = 0;
+// function slider(){
+//     var img = document.getElementById("img-slide");
+//     img.src=arr[index];
+//     index++;
+//     if(index == arr.length)
+//         index = 0;
+//     window.setTimeout("slider()", 2000);
+// }
